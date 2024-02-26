8c5e9e10979574d3c9bd9e01714435f464b9be24
cielt <chiayin.l.tu@gmail.com>
2024-02-26 16:52:40 -0500

    style photo book hero area * add CSS custom properties, cont'd * activate nav menu

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
-import { setupCounter } from './counter.js';
+// Facing Change JS
 
-setupCounter(document.querySelector('#counter'));
+const $globalMenuBtns = document.querySelectorAll(
+  '#global-menu-btn, #close-menu-btn, .menu-overlay'
+);
+
+const $siteNavBlock = document.querySelector('.site-nav-block');
+
+console.log($globalMenuBtns);
+console.log($siteNavBlock);
+
+if ($globalMenuBtns.length) {
+  $globalMenuBtns.forEach(($btn) => {
+    $btn.addEventListener('click', (ev) => {
+      ev.preventDefault();
+      document.body.classList.toggle('nav-open');
+      document.body.classList.toggle('nav-closed');
+      $siteNavBlock.scrollTop(0);
+    });
+  });
+}
